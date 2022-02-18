aa6cdd7d9c71538d3a0925afe6a04b3582692d4f
george-arias <georgearias08@gmail.com>
2022-02-17 18:39:52 -0600

    css done

--- server.js
+++ server.js
@@ -127,14 +127,6 @@ app.post("/login", (req, res) => {
     })
 })
 
-app.get("/lyrics", async (req, res) => {
-  const lyrics =
-    (await lyricsFinder(req.query.artist, req.query.track)) || "No Lyrics Found"
-  res.json({ lyrics })
-})
-
-
-
 
 app.listen(PORT, ()=> {
     console.log('listening on port 3001')
